b6e5c46ba53a4728333043f8c9dd510a1d8ae679
Milton <Tim150492>
2020-03-29 14:29:36 -0300

    Primeiro Commit

---
+++ src/test/java/com/unboxculture/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unboxculture.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
